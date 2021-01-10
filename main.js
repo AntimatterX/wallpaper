c9f511e58e0a26b82c790f8e561129dbaabc6435
AntimatterX <51862109+AntimatterX@users.noreply.github.com>
2021-01-10 20:58:36 +0900

    Update main.js

--- main.js
+++ main.js
@@ -12,7 +12,11 @@
         	min: 0,
         	max: urls.length - 1,
         	change: function(idx){
+        		try {
         		amx.setWallpaper(urls[idx]);
+        	}catch(e){
+        		alert(e);
+        	};
         	}
         });
     });
