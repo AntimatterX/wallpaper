ead1aac474f65de44b0afb5426835e2709e4a54f
AntimatterX <51862109+AntimatterX@users.noreply.github.com>
2021-01-10 20:50:57 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,16 @@
     'use strict';
     //--------------------------------------------------
     $.get("https://antimatterx.github.io/wallpaper/src/wallpapers.txt").done(function(d) {
-        console.log(arguments);
+        var urls = d.split("\n"),
+        h = $("<div>").appendTo("body");
+        amx.addInputRange(h[0], {
+        	title: "壁紙のインデックス",
+        	min: 0,
+        	max: urls.length - 1,
+        	change: function(idx){
+        		amx.setWallpaper(urls[idx]);
+        	}
+        });
     });
     //--------------------------------------------------
 })();
