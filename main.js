f7bba3d9363fb8e01e8d3cbf9854dd2adf460c76
AntimatterX <51862109+AntimatterX@users.noreply.github.com>
2021-01-10 20:47:28 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
     'use strict';
     //--------------------------------------------------
     $.get("https://antimatterx.github.io/wallpaper/src/wallpapers.txt").done(function(d) {
-        console.log(d);
+        console.log(arguments);
     });
     //--------------------------------------------------
 })();
