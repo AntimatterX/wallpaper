5a54a82538a8622454c78c1f3474af4470bca52e
AntimatterX <51862109+AntimatterX@users.noreply.github.com>
2021-01-10 22:29:11 +0900

    Update main.js

--- main.js
+++ main.js
@@ -6,22 +6,15 @@
         	return v.length > 0;
         }),
         h = $("<div>").appendTo("body");
-        urls.forEach(function(v){
-        	$("<img>", {
-        		src: v,
-        		crossorigin: "anonymous"
-        	}).appendTo(h);
-        });
-        /*amx.addInputRange(h[0], {
+        amx.addInputRange(h[0], {
         	title: "壁紙のインデックス",
         	value: 0,
         	min: 0,
         	max: urls.length - 1,
         	change: function(idx){
         		amx.setWallpaper(urls[idx]);
-        		alert($("body").css("background-image"));
         	}
-        });*/
+        });
     });
     //--------------------------------------------------
 })();
