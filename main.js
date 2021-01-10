4bc09b75bd4244f849d07801fce9b2d7bb8fa079
AntimatterX <51862109+AntimatterX@users.noreply.github.com>
2021-01-10 20:44:38 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+(function() {
+    'use strict';
+    //--------------------------------------------------
+    $.get("https://antimatterx.github.io/wallpaper/src/wallpapers.txt").done(function(d) {
+        console.log(d);
+    });
+    //--------------------------------------------------
+})();
