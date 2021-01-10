cfe8df3f5e2adc4919df0fd02619d9d308b11fed
AntimatterX <51862109+AntimatterX@users.noreply.github.com>
2021-01-10 23:29:22 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 (function() {
     'use strict';
     //--------------------------------------------------
+    $("body script").attr( "crossorigin","anonymous");
     $.get("https://antimatterx.github.io/wallpaper/src/wallpapers.txt").done(function(d) {
         var urls = d.split("\n").filter(function(v){
         	return v.length > 0;
@@ -9,8 +10,8 @@
         urls.forEach(function(v){
         	$("<img>", {
         		src: v,
-        		//crossorigin: "anonymous"
-        	}).appendTo(h);
+        		 crossorigin:"anonymous"
+        	}).appendTo(h)
         });
         /*amx.addInputRange(h[0], {
         	title: "壁紙のインデックス",
@@ -19,6 +20,7 @@
         	max: urls.length - 1,
         	change: function(idx){
         		amx.setWallpaper(urls[idx]);
+        		alert($("body").css("background-image"));
         	}
         });*/
     });
