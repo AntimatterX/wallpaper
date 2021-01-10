98873fc74d7938bf7c00aca285a663e352a690f2
AntimatterX <51862109+AntimatterX@users.noreply.github.com>
2021-01-11 03:30:39 +0900

    Update main.js

--- main.js
+++ main.js
@@ -21,14 +21,21 @@
             change: function(idx) {
                 amx.setWallpaper("https://i.imgur.com/" + urls[idx]);
             }
+        })),
+        inputInterval = $(amx.addInputRange(h[0], {
+            title: "切り替え秒数",
+            value: 2.5,
+            min: 1,
+            max: 10,
+            step: 0.5
         }));
     amx.addInputBool(h[0], {
         title: "自動切り替え",
         change: function(flag) {
             if (flag) si = setInterval(function() {
-                var nextIdx = inputIndex.val() + 1;
-                inputIndex.val(nextIdx > urls.length ? 0 : nextIdx);
-            }, 2500);
+                var nextIdx = Number(inputIndex.val()) + 1;
+                amx.triggerEvent(inputIndex.val(nextIdx >= urls.length ? 0 : nextIdx)[0], "change");
+            }, inputInterval.val() * 1000);
             else clearInterval(si);
         }
     });
